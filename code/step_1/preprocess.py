3909bcd13cb0419228d305c533cbdf1e4af7bdfe
SteffRhes <steffrhes@pm.me>
2026-04-08 18:02:36 +0200

    code/step_1/preprocess.py: implement proper verticals output, add small test file

--- code/step_1/preprocess.py
+++ code/step_1/preprocess.py
@@ -1,13 +1,19 @@
+import re
+
 import nltk
-
-
 from nltk.tokenize import sent_tokenize
 
 
-with open("/veld/input/step_0_The_Project_Gutenberg_Etext_Fairy_Tales,_by_the_Grimm_Brothers.txt", "r") as f:
+with open("/veld/input/step_0_test.txt", "r") as f:
     text = f.read()
-
-text = text.replace("\n", " ")
-with open("/veld/output/step_1_The_Project_Gutenberg_Etext_Fairy_Tales,_by_the_Grimm_Brothers.txt", "w") as f:
-    f.write(text)
+text = re.sub(r"\s+", " ", text)
+sentence_list = sent_tokenize(text, "german")
+text_output = ""
+for sentence in sentence_list:
+    token_list = nltk.word_tokenize(sentence, "german")
+    text_output += "\n"
+    for token in token_list:
+        text_output += token + "\n"
+with open("/veld/output/step_1_test.txt", "w") as f:
+    f.write(text_output)
 
